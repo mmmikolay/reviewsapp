063637d7ad20ee4b0bfec0208ef82c81561a8488
tuncerilk@gmail.com <tuncerilk@gmail.com>
2020-03-11 21:09:25 +0300

    Updates

---
+++ precache-manifest.fedcb23f9a0c892d670316985f87decb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc4a936f5819d0e4dd936b732d3db577",
+    "url": "/reviewsapp/index.html"
+  },
+  {
+    "revision": "0bdee19b96c36cb1ee55",
+    "url": "/reviewsapp/static/css/main.bbe4f3e0.chunk.css"
+  },
+  {
+    "revision": "60f69a1d49dbc90b29e7",
+    "url": "/reviewsapp/static/js/2.1f38468b.chunk.js"
+  },
+  {
+    "revision": "5d10d3f87f76cdcd27df5516b60fcaf9",
+    "url": "/reviewsapp/static/js/2.1f38468b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bdee19b96c36cb1ee55",
+    "url": "/reviewsapp/static/js/main.109c00ae.chunk.js"
+  },
+  {
+    "revision": "38b881d2e33cac49ea26",
+    "url": "/reviewsapp/static/js/runtime-main.08ad6206.js"
+  },
+  {
+    "revision": "5767180af9e78922c36fc0f41311ce84",
+    "url": "/reviewsapp/static/media/Map.5767180a.png"
+  }
+]);
