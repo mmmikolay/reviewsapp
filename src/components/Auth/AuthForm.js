9c80dd3013782423448096151f798df0f5e67705
tuncerilk@gmail.com <tuncerilk@gmail.com>
2020-03-25 21:17:20 +0300

    Solving auth issues

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -45,7 +45,7 @@ const AuthForm = () => {
                 }
                 return res.json();
             }).then(resData => {
-                if(resData.data.login.success === true){
+                if(resData.data.login.success){
                     document.cookie = 'signedin=true';
                     // history.push("/admin_panel")
                 }
